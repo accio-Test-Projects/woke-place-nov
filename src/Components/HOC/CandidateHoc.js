caee3cb8407bfbfbae40eaa5fa699fa65b4e36f5
sahil jaryal <sahil.jaryal@shiryam.com>
2022-11-15 22:43:09 +0530

    nav barr added

---
+++ src/Components/HOC/CandidateHoc.js
@@ -0,0 +1,206 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function CandidateHoc({ children }) {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [value, setValue] = React.useState(0);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: { xs: "none", md: "block" },
+        }}
+      >
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                LOGO
+              </Typography>
+
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">{page}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+              <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href=""
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                LOGO
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                {pages.map((page) => (
+                  <Button
+                    key={page}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    {page}
+                  </Button>
+                ))}
+              </Box>
+
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </Toolbar>
+          </Container>
+          <div></div>
+        </AppBar>
+      </Box>
+      <Box
+        display={{
+          xs: "block",
+          md: "none",
+          position: "fixed",
+          bottom: "0px",
+          width: "100%",
+          background: "white",
+          zIndex: "2",
+        }}
+      >
+        <Box >
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          >
+            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+          </BottomNavigation>
+        </Box>
+      </Box>
+      {children}
+    </>
+  );
+}
+
+export default CandidateHoc;
--- src/Navs/index.js
+++ src/Navs/index.js
@@ -18,9 +18,11 @@ import EmployerProfile from "../Components/Employer/Profile";
 import EmployerJobs from "../Components/Employer/Jobs";
 import EmployerConversation from "../Components/Employer/Conversation";
 import Applicants from "../Components/Employer/Applicants";
+import CandidateHoc from "../Components/HOC/CandidateHoc";
+import EmployerHoc from "../Components/HOC/EmployerHoc";
+
 function Navs() {
   const CandidateProtactedRoutes = () => {
-
     if ("a" === "a") {
       return <Outlet />;
     } else {
@@ -34,38 +36,87 @@ function Navs() {
     } else {
       return <Navigate to="/" />;
     }
-  }
-
+  };
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/candidate/auth" element={<AuthPage type='candidate' />} />
-        <Route path="/employer/auth" element={<AuthPage type='employer' />} />
+        <Route path="/candidate/auth" element={<AuthPage type="candidate" />} />
+        <Route path="/employer/auth" element={<AuthPage type="employer" />} />
 
         <Route element={<CandidateProtactedRoutes />}>
           <Route
             path="/candidate/onboarding"
             element={<CandidateOnboarding />}
           />
-          <Route path="candidate/profile" element={<CandidateProfile />} />
-          <Route path="candidate/jobs" element={<CandidateJobs />} />
+          <Route
+            path="candidate/profile"
+            element={
+              <CandidateHoc>
+                <CandidateProfile />
+              </CandidateHoc>
+            }
+          />
+          <Route
+            path="candidate/jobs"
+            element={
+              <CandidateHoc>
+                <CandidateJobs />
+              </CandidateHoc>
+            }
+          />
           <Route
             path="candidate/conversation"
-            element={<CandidateConversation />}
+            element={
+              <CandidateHoc>
+                <CandidateConversation />
+              </CandidateHoc>
+            }
           />
-          <Route path="candidate/application" element={<Applications />} />
+          <Route
+            path="candidate/application"
+            element={
+              <CandidateHoc>
+                <Applications />
+              </CandidateHoc>
+            }
+          />
         </Route>
         <Route element={<EmployerProtactedRoutes />}>
           <Route path="/employer/onboarding" element={<EmployerOnboarding />} />
-          <Route path="employer/profile" element={<EmployerProfile />} />
-          <Route path="employer/jobs" element={<EmployerJobs />} />
+          <Route
+            path="employer/profile"
+            element={
+              <EmployerHoc>
+                <EmployerProfile />
+              </EmployerHoc>
+            }
+          />
+          <Route
+            path="employer/jobs"
+            element={
+              <EmployerHoc>
+                <EmployerJobs />
+              </EmployerHoc>
+            }
+          />
           <Route
             path="employer/conversation"
-            element={<EmployerConversation />}
+            element={
+              <EmployerHoc>
+                <EmployerConversation />
+              </EmployerHoc>
+            }
           />
-          <Route path="employer/applicants" element={<Applicants />} />
+          <Route
+            path="employer/applicants"
+            element={
+              <EmployerHoc>
+                <Applicants />
+              </EmployerHoc>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
